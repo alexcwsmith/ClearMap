36ef5c600d2dc8b52dfe5f44a5f97e512976036c
Alex Smith <smitaw@musc.edu>
2022-04-26 11:04:31 -0400

    Modified param/process templates

--- ClearMap/Scripts/parameter_file_template.py
+++ ClearMap/Scripts/parameter_file_template.py
@@ -6,7 +6,10 @@ Example script to set up the parameters for the image processing pipeline
 ######################### Import modules
 
 import os, numpy, math
+import pandas as pd
+from scipy.stats import zscore
 
+os.chdir('/d1/software/ClearMap/')
 import ClearMap.Settings as settings
 import ClearMap.IO as io
 
@@ -19,38 +22,50 @@ from ClearMap.Analysis.Voxelization import voxelize
 from ClearMap.Analysis.Statistics import thresholdPoints
 from ClearMap.Utils.ParameterTools import joinParameter
 from ClearMap.Analysis.Label import labelToName
-
+from multiprocessing import Process
+import matplotlib.pyplot as mplt
 
 ######################### Data parameters
 
 #Directory to save all the results, usually containing the data for one sample
-BaseDirectory = '/home/yourname/experiment/sampleID';
+sampleName='LSM1R'
+signalChannel='fos'
+region='all'
+BaseDirectory = '/d2/studies/CM2/SmartSPIM/ReichelLab/'+sampleName+'_Stitched_stacks/';
 
 #Data File and Reference channel File, usually as a sequence of files from the microscope
-#Use \d{4} for 4 digits in the sequence for instance. As an example, if you have cfos-Z0001.ome.tif :
+#Use \d{4} for 4 digits in the sequence for instance. As an example, if you have signal-Z0001.ome.tif :
 #os.path.join() is used to join the BaseDirectory path and the data paths:
-cFosFile = os.path.join(BaseDirectory, 'channel1/cfos-Z\d{4}.ome.tif');
-AutofluoFile = os.path.join(BaseDirectory, 'channel2/ref-Z\d{4}.ome.tif');
+signalFile = os.path.join(BaseDirectory, sampleName+'_Stitched_'+signalChannel+'.ome.tif');
+AutofluoFile = os.path.join(BaseDirectory, sampleName+'_Stitched_auto.ome.tif');
 
 #Specify the range for the cell detection. This doesn't affect the resampling and registration operations
-cFosFileRange = {'x' : all, 'y' : (180, 2560), 'z' : all};
+if region.lower()=='all':   
+    signalFileRange = {'x' : all, 'y' : all, 'z' : all};
+elif region.lower()=='pfc':
+    signalFileRange = {'x' : (1200,1500), 'y' : (300,600), 'z' : (400,500)};
+elif region.lower()=='hpc':
+    signalFileRange = {'x' : (400,700), 'y' : (2700,3000), 'z' : (500,600)};
+elif region.lower()=='pir':
+    signalFileRange = {'x' : (1600,1900), 'y' : (1150,1450), 'z' : (1000,1100)};
+
+
 
 #Resolution of the Raw Data (in um / pixel)
-OriginalResolution = (4.0625, 4.0625, 3);
+OriginalResolution = (4,4,4);
 
 #Orientation: 1,2,3 means the same orientation as the reference and atlas files.
 #Flip axis with - sign (eg. (-1,2,3) flips x). 3D Rotate by swapping numbers. (eg. (2,1,3) swaps x and y)
 FinalOrientation = (1,2,3);
 
 #Resolution of the Atlas (in um/ pixel)
-AtlasResolution = (25, 25, 25);
+AtlasResolution = (39,39,39);
 
 #Path to registration parameters and atlases
-PathReg        = '/home/mtllab/Documents/warping';
-AtlasFile      = os.path.join(PathReg, 'half_template_25_right_fullWD.tif');
-AnnotationFile = os.path.join(PathReg, 'annotation_25_right_fullWD.tif');
-
-
+PathReg        = '/d2/studies/ClearMap/Parameter_files/';
+AtlasFile      = '/d2/studies/CM2/SmartSPIM/ReichelLab/WHS_SD_RatAtlas_Cropped.ome.tif';
+AnnotationFile = '/d2/studies/CM2/SmartSPIM/ReichelLab/WHS_SD_RatAnnotations_Cropped.ome.tif';
+regionIndex = '/d1/software/ClearMap/ClearMap/Data/Waxholm_SD_rat_atlas_annotationLabels.csv'
 
 ######################### Cell Detection Parameters using custom filters
 
@@ -69,25 +84,25 @@ correctIlluminationParameter = {
 
 #Remove the background with morphological opening (optimised for spherical objects)
 removeBackgroundParameter = {
-    "size"    : (7,7),  # size in pixels (x,y) for the structure element of the morphological opening
-    "save"    : None,     # file name to save result of this operation
+    "size"    : (12,12),  # size in pixels (x,y) for the structure element of the morphological opening
+    "save"    : None, #os.path.join(BaseDirectory, 'Background/background\d{4}.ome.tif'), # file name to save result of this operation
     "verbose" : True  # print / plot information about this step       
 }
 
 #Difference of Gaussians filter: to enhance the edges. Useful if the objects have a non smooth texture (eg: amyloid deposits)
 filterDoGParameter = {
-    "size"    : None,        # (tuple or None)      size for the DoG filter in pixels (x,y,z) if None, do not correct for any background
+    "size"    : (6,6,6),        # (tuple or None)      size for the DoG filter in pixels (x,y,z) if None, do not correct for any background
     "sigma"   : None,        # (tuple or None)      std of outer Gaussian, if None automatically determined from size
     "sigma2"  : None,        # (tuple or None)      std of inner Gaussian, if None automatically determined from size
-    "save"    : None,        # (str or None)        file name to save result of this operation if None dont save to file 
-    "verbose" : True      # (bool or int)        print / plot information about this step
+    "save"    : None, #os.path.join(BaseDirectory, 'DoG/DoG\d{4}.ome.tif'),        # (str or None)        file name to save result of this operation if None dont save to file 
+    "verbose" : True         # (bool or int)        print / plot information about this step
 }
 
 #Extended maxima: if the peak intensity in the object is surrounded by smaller peaks: avoids overcounting "granular" looking objects
 findExtendedMaximaParameter = {
     "hMax"      : None,            # (float or None)     h parameter (for instance 20) for the initial h-Max transform, if None, do not perform a h-max transform
-    "size"      : 5,             # (tuple)             size for the structure element for the local maxima filter
-    "threshold" : 0,        # (float or None)     include only maxima larger than a threshold, if None keep all local maxima
+    "size"      : (6,6,6),             # (tuple)             size for the structure element for the local maxima filter
+    "threshold" : 8,        # (float or None)     include only maxima larger than a threshold, if None keep all local maxima
     "save"      : None,         # (str or None)       file name to save result of this operation if None dont save to file 
     "verbose"   : True       # (bool or int)       print / plot information about this step
 }
@@ -95,16 +110,16 @@ findExtendedMaximaParameter = {
 #If no cell shape detection and the maximum intensity is not at the gravity center of the object, look for a peak intensity around the center of mass. 
 findIntensityParameter = {
     "method" : 'Max',       # (str, func, None)   method to use to determine intensity (e.g. "Max" or "Mean") if None take intensities at the given pixels
-    "size"   : (3,3,3)      # (tuple)             size of the search box on which to perform the *method*
+    "size"   : (6,6,4)      # (tuple)             size of the search box on which to perform the *method*
 }
 
 #Object volume detection. The object is painted by a watershed, until reaching the intensity threshold, based on the background subtracted image
 detectCellShapeParameter = {
-    "threshold" : 700,     # (float or None)      threshold to determine mask. Pixels below this are background if None no mask is generated
-    "save"      : None,        # (str or None)        file name to save result of this operation if None dont save to file 
+    "threshold" : 125,     # (float or None)      threshold to determine mask. Pixels below this are background if None no mask is generated
+    "save"      : None, #os.path.join(BaseDirectory, 'cellShape/cellShape\d{4}.ome.tif'), # (str or None)        file name to save result of this operation if None dont save to file 
     "verbose"   : True      # (bool or int)        print / plot information about this step if None take intensities at the given pixels
 }
-
+#to save: os.path.join(BaseDirectory, 'cellshape/cellshape\d{4}.ome.tif')
 
 ## Parameters for cell detection using spot detection algorithm 
 detectSpotsParameter = {
@@ -123,7 +138,7 @@ detectSpotsParameter = {
 #################### Heat map generation
 
 ##Voxelization: file name for the output:
-VoxelizationFile = os.path.join(BaseDirectory, 'points_voxelized.tif');
+VoxelizationFile = os.path.join(BaseDirectory, sampleName+'_points_voxelized.tif');
 
 # Parameter to calculate the density of the voxelization
 voxelizeParameter = {
@@ -145,19 +160,19 @@ voxelizeParameter = {
 
 #Processes to use for Resampling (usually twice the number of physical processors)
 ResamplingParameter = {
-    "processes": 12 
+    "processes": 18, 
 };
 
 
 #Stack Processing Parameter for cell detection
 StackProcessingParameter = {
     #max number of parallel processes. Be careful of the memory footprint of each process!
-    "processes" : 6,
+    "processes" : 16,
    
     #chunk sizes: number of planes processed at once
-    "chunkSizeMax" : 100,
-    "chunkSizeMin" : 50,
-    "chunkOverlap" : 32,
+    "chunkSizeMax" : 120,
+    "chunkSizeMin" : 20,
+    "chunkOverlap" : 10,
 
     #optimize chunk size and number to number of processes to limit the number of cycles
     "chunkOptimization" : True,
@@ -176,47 +191,47 @@ StackProcessingParameter = {
 ######################## Run Parameters, usually you don't need to change those
 
 
-### Resample Fluorescent and CFos images
-# Autofluorescent cFos resampling for aquisition correction
+### Resample Fluorescent and signal images
+# Autofluorescent signal resampling for aquisition correction
 
-ResolutionAffineCFosAutoFluo =  (16, 16, 16);
+ResolutionAffinesignalAutoFluo =  (16, 16, 16);
 
-CorrectionResamplingParameterCfos = ResamplingParameter.copy();
+CorrectionResamplingParametersignal = ResamplingParameter.copy();
 
-CorrectionResamplingParameterCfos["source"] = cFosFile;
-CorrectionResamplingParameterCfos["sink"]   = os.path.join(BaseDirectory, 'cfos_resampled.tif');
+CorrectionResamplingParametersignal["source"] = signalFile;
+CorrectionResamplingParametersignal["sink"]   = os.path.join(BaseDirectory, sampleName+'_'+signalChannel+'_resampled.tif');
     
-CorrectionResamplingParameterCfos["resolutionSource"] = OriginalResolution;
-CorrectionResamplingParameterCfos["resolutionSink"]   = ResolutionAffineCFosAutoFluo;
+CorrectionResamplingParametersignal["resolutionSource"] = OriginalResolution;
+CorrectionResamplingParametersignal["resolutionSink"]   = ResolutionAffinesignalAutoFluo;
 
-CorrectionResamplingParameterCfos["orientation"] = FinalOrientation;
+CorrectionResamplingParametersignal["orientation"] = FinalOrientation;
    
    
    
 #Files for Auto-fluorescence for acquisition movements correction
-CorrectionResamplingParameterAutoFluo = CorrectionResamplingParameterCfos.copy();
+CorrectionResamplingParameterAutoFluo = CorrectionResamplingParametersignal.copy();
 CorrectionResamplingParameterAutoFluo["source"] = AutofluoFile;
-CorrectionResamplingParameterAutoFluo["sink"]   = os.path.join(BaseDirectory, 'autofluo_for_cfos_resampled.tif');
+CorrectionResamplingParameterAutoFluo["sink"]   = os.path.join(BaseDirectory, sampleName+'_autofluo_for_signal_resampled.tif');
    
 #Files for Auto-fluorescence (Atlas Registration)
 RegistrationResamplingParameter = CorrectionResamplingParameterAutoFluo.copy();
-RegistrationResamplingParameter["sink"]            =  os.path.join(BaseDirectory, 'autofluo_resampled.tif');
+RegistrationResamplingParameter["sink"]            =  os.path.join(BaseDirectory, sampleName+'_autofluo_resampled.tif');
 RegistrationResamplingParameter["resolutionSink"]  = AtlasResolution;
    
 
-### Align cFos and Autofluo
+### Align signal and Autofluo
 
 CorrectionAlignmentParameter = {            
     #moving and reference images
-    "movingImage" : os.path.join(BaseDirectory, 'autofluo_for_cfos_resampled.tif'),
-    "fixedImage"  : os.path.join(BaseDirectory, 'cfos_resampled.tif'),
+    "movingImage" : os.path.join(BaseDirectory, sampleName+'_autofluo_for_signal_resampled.tif'),
+    "fixedImage"  : os.path.join(BaseDirectory, sampleName+'_'+signalChannel+'_resampled.tif'),
     
     #elastix parameter files for alignment
     "affineParameterFile"  : os.path.join(PathReg, 'Par0000affine_acquisition.txt'),
     "bSplineParameterFile" : None,
     
     #directory of the alignment result
-    "resultDirectory" :  os.path.join(BaseDirectory, 'elastix_cfos_to_auto')
+    "resultDirectory" :  os.path.join(BaseDirectory, 'elastix_signal_to_auto')
     }; 
   
 
@@ -229,7 +244,7 @@ RegistrationAlignmentParameter["resultDirectory"] = os.path.join(BaseDirectory, 
     
 #moving and reference images
 RegistrationAlignmentParameter["movingImage"]  = AtlasFile;
-RegistrationAlignmentParameter["fixedImage"]   = os.path.join(BaseDirectory, 'autofluo_resampled.tif');
+RegistrationAlignmentParameter["fixedImage"]   = os.path.join(BaseDirectory, sampleName+'_autofluo_resampled.tif');
 
 #elastix parameter files for alignment
 RegistrationAlignmentParameter["affineParameterFile"]  = os.path.join(PathReg, 'Par0000affine.txt');
@@ -239,35 +254,35 @@ RegistrationAlignmentParameter["bSplineParameterFile"] = os.path.join(PathReg, '
 
 # result files for cell coordinates (csv, vtk or ims)
 SpotDetectionParameter = {
-    "source" : cFosFile,
-    "sink"   : (os.path.join(BaseDirectory, 'cells-allpoints.npy'),  os.path.join(BaseDirectory,  'intensities-allpoints.npy')),
+    "source" : signalFile,
+    "sink"   : (os.path.join(BaseDirectory, sampleName+'_cells-allpoints.npy'),  os.path.join(BaseDirectory,  sampleName+'_intensities-allpoints.npy')),
     "detectSpotsParameter" : detectSpotsParameter
 };
-SpotDetectionParameter = joinParameter(SpotDetectionParameter, cFosFileRange)
+SpotDetectionParameter = joinParameter(SpotDetectionParameter, signalFileRange)
 
 ImageProcessingParameter = joinParameter(StackProcessingParameter, SpotDetectionParameter);
 
-FilteredCellsFile = (os.path.join(BaseDirectory, 'cells.npy'), os.path.join(BaseDirectory,  'intensities.npy'));
+FilteredCellsFile = (os.path.join(BaseDirectory, sampleName+'_cells.npy'), os.path.join(BaseDirectory, sampleName+'_intensities.npy'));
 
-TransformedCellsFile = os.path.join(BaseDirectory, 'cells_transformed_to_Atlas.npy')
+TransformedCellsFile = os.path.join(BaseDirectory, sampleName+'_cells_transformed_to_Atlas.npy')
 
 ### Transform points from Original c-Fos position to autofluorescence
 
 ## Transform points from original to corrected
 # downscale points to referenece image size
 
-CorrectionResamplingPointsParameter = CorrectionResamplingParameterCfos.copy();
-CorrectionResamplingPointsParameter["pointSource"] = os.path.join(BaseDirectory, 'cells.npy');
-CorrectionResamplingPointsParameter["dataSizeSource"] = cFosFile;
+CorrectionResamplingPointsParameter = CorrectionResamplingParametersignal.copy();
+CorrectionResamplingPointsParameter["pointSource"] = os.path.join(BaseDirectory, sampleName+'_cells.npy');
+CorrectionResamplingPointsParameter["dataSizeSource"] = signalFile;
 CorrectionResamplingPointsParameter["pointSink"]  = None;
 
 CorrectionResamplingPointsInverseParameter = CorrectionResamplingPointsParameter.copy();
-CorrectionResamplingPointsInverseParameter["dataSizeSource"] = cFosFile;
+CorrectionResamplingPointsInverseParameter["dataSizeSource"] = signalFile;
 CorrectionResamplingPointsInverseParameter["pointSink"]  = None;
 
 ## Transform points from corrected to registered
 # downscale points to referenece image size
 RegistrationResamplingPointParameter = RegistrationResamplingParameter.copy();
-RegistrationResamplingPointParameter["dataSizeSource"] = cFosFile;
+RegistrationResamplingPointParameter["dataSizeSource"] = signalFile;
 RegistrationResamplingPointParameter["pointSink"]  = None;
 
--- ClearMap/Scripts/process_template.py
+++ ClearMap/Scripts/process_template.py
@@ -2,15 +2,15 @@
 """
 Template to run the processing pipeline
 """
-
 #load the parameters:
-execfile('/home/yourname/experiment/parameter_file_sampleID.py')
-
+sampleName='LSM1R'
+testing=False
+execfile('/d2/studies/CM2/SmartSPIM/ReichelLab/'+sampleName+'_Stitched_stacks/clearMap_parameters_'+sampleName+'.py')
 
 #resampling operations:
 #######################
 #resampling for the correction of stage movements during the acquisition between channels:
-resampleData(**CorrectionResamplingParameterCfos);
+resampleData(**CorrectionResamplingParametersignal);
 resampleData(**CorrectionResamplingParameterAutoFluo);
 
 #Downsampling for alignment to the Atlas:
@@ -25,6 +25,7 @@ resultDirectory  = alignData(**CorrectionAlignmentParameter);
 #alignment to the Atlas:
 resultDirectory  = alignData(**RegistrationAlignmentParameter);
 
+#execfile('/d2/studies/CM2/SmartSPIM/ReichelLab/'+sampleName+'_Stitched_stacks/clearMap_parameters_'+sampleName+'.py')
 
 #Cell detection:
 ################
@@ -35,21 +36,35 @@ detectCells(**ImageProcessingParameter);
 #Loading the results:
 points, intensities = io.readPoints(ImageProcessingParameter["sink"]);
 
+topPerc = numpy.percentile(intensities[:,3], 99.9)
+fig = mplt.figure()
+vox = intensities[intensities[:,3]<topPerc][:,3]
+mplt.hist(vox, bins=5)
+#mplt.show()
+fig.savefig(os.path.join(BaseDirectory, sampleName+'_voxelSizeHistogram_'+region+'.png'))
+
+
 #Thresholding: the threshold parameter is either intensity or size in voxel, depending on the chosen "row"
 #row = (0,0) : peak intensity from the raw data
 #row = (1,1) : peak intensity from the DoG filtered data
 #row = (2,2) : peak intensity from the background subtracted data
 #row = (3,3) : voxel size from the watershed
-points, intensities = thresholdPoints(points, intensities, threshold = (20, 900), row = (3,3));
+print("Unfiltered # of points: " + str(points.shape[0]))
+minSize=12
+maxSize=200
+print("Filtering out " + str(intensities[intensities[:,3]<minSize].shape[0]) + " points smaller than " + str(minSize) + " pixels")
+print("Filtering out " + str(intensities[intensities[:,3]>maxSize].shape[0]) + " points larger than " + str(maxSize) + "pixels")
+points, intensities = thresholdPoints(points, intensities, threshold = (minSize, maxSize), row = (3,3));
 io.writePoints(FilteredCellsFile, (points, intensities));
 
 
 ## Check Cell detection (For the testing phase only, remove when running on the full size dataset)
-#######################
-#import ClearMap.Visualization.Plot as plt;
-#pointSource= os.path.join(BaseDirectory, FilteredCellsFile[0]);
-#data = plt.overlayPoints(cFosFile, pointSource, pointColor = None, **cFosFileRange);
-#io.writeData(os.path.join(BaseDirectory, 'cells_check.tif'), data);
+#########################
+if testing:
+    import ClearMap.Visualization.Plot as plt;
+    pointSource= os.path.join(BaseDirectory, FilteredCellsFile[0]);
+    data = plt.overlayPoints(signalFile, pointSource, pointColor = None, **signalFileRange);
+    io.writeData(os.path.join(BaseDirectory, sampleName+'cells_check_'+region+'_'+str(minSize)+'-'+str(maxSize)+'px.tif'), data);
 
 
 # Transform point coordinates
@@ -75,37 +90,39 @@ intensities = io.readPoints(FilteredCellsFile[1])
 #Without weigths:
 vox = voxelize(points, AtlasFile, **voxelizeParameter);
 if not isinstance(vox, basestring):
-  io.writeData(os.path.join(BaseDirectory, 'cells_heatmap.tif'), vox.astype('int32'));
+  io.writeData(os.path.join(BaseDirectory, sampleName+'_cells_heatmap_vox15.tif'), vox.astype('int32'));
+#
 
 #With weigths from the intensity file (here raw intensity):
 voxelizeParameter["weights"] = intensities[:,0].astype(float);
 vox = voxelize(points, AtlasFile, **voxelizeParameter);
 if not isinstance(vox, basestring):
-  io.writeData(os.path.join(BaseDirectory, 'cells_heatmap_weighted.tif'), vox.astype('int32'));
+  io.writeData(os.path.join(BaseDirectory, sampleName+'_cells_heatmap_weighted_vox15.tif'), vox.astype('int32'));
+#
 
-
-
-
-
-#Table generation:
-##################
-#With integrated weigths from the intensity file (here raw intensity):
-ids, counts = countPointsInRegions(points, labeledImage = AnnotationFile, intensities = intensities, intensityRow = 0);
-table = numpy.zeros(ids.shape, dtype=[('id','int64'),('counts','f8'),('name', 'a256')])
-table["id"] = ids;
-table["counts"] = counts;
-table["name"] = labelToName(ids);
-io.writeTable(os.path.join(BaseDirectory, 'Annotated_counts_intensities.csv'), table);
+###Counts analysis
+regs = pd.read_csv(regionIndex, index_col=0)
 
 #Without weigths (pure cell number):
-ids, counts = countPointsInRegions(points, labeledImage = AnnotationFile, intensities = None);
+ids, counts = countPointsInRegions(points, labeledImage = AnnotationFile, intensities = None, collapse=None)
 table = numpy.zeros(ids.shape, dtype=[('id','int64'),('counts','f8'),('name', 'a256')])
-table["id"] = ids;
-table["counts"] = counts;
-table["name"] = labelToName(ids);
-io.writeTable(os.path.join(BaseDirectory, 'Annotated_counts.csv'), table);
+table["id"] = ids
+table["counts"] = counts
+table["name"] = labelToName(ids)
+df = pd.DataFrame(table)
+df.set_index('id', inplace=True)
+df.sort_values(by='counts', ascending=False, inplace=True)
+df.to_csv(os.path.join(BaseDirectory, sampleName + '_Annotated_counts.csv'))
 
-
+ids, counts = countPointsInRegions(points, labeledImage = AnnotationFile, intensities = None, collapse=True)
+table = numpy.zeros(ids.shape, dtype=[('id','int64'),('counts','f8'),('name', 'a256')])
+table["id"] = ids
+table["counts"] = counts
+table["name"] = labelToName(ids)
+df = pd.DataFrame(table)
+df.set_index('id', inplace=True)
+df.sort_values(by='counts', ascending=False, inplace=True)
+df.to_csv(os.path.join(BaseDirectory, sampleName + '_Annotated_counts_collapse.csv'))
 
 #####################
 #####################
